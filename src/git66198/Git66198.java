19d7690dbd2e9654e2da01e2304af2742cf5a75d
galbe <galbe@192.168.1.70>
2021-02-19 09:44:37 -0600

    Primer commit

---
+++ src/git66198/Git66198.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git66198;
+
+public class Git66198 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre= "Miguel Angel Dzib Alayola";
+        String matricula= "66198";
+        System.out.println( " Soy " + nombre + " Y mi matricula es " + matricula );
+        // ACABO DE CREAR MI REPOSITORIO LOCAL EN TEAM
+           
+    }
+    
+}
